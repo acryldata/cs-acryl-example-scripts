73bc65c013dbd7b2b592138cfeb1bc58e34e711c
Ethan Cartwright <ethan.cartwright@acryl.io>
2024-08-02 12:58:11 -0400

    add untested chatGPT type annotations

--- circuit_break_examples/examples.py
+++ circuit_break_examples/examples.py
@@ -1,19 +1,25 @@
 import os
 from datahub.ingestion.graph.client import DatahubClientConfig, DataHubGraph
-from graphql.gql_variable_definitions import *
-
+from graphql.gql_variable_definitions import (
+    get_scroll_across_lineage_vars,
+    get_dataset_assertions_vars,
+    get_add_tag_vars,
+    get_remove_tag_vars,
+    get_assertion_details_vars,
+    get_run_assertion_vars,
+)
+from typing import Dict, Any
 
 GRAPHQL_QUERY_FOLDER_PATH = "/Users/ethancartwright/Desktop/customer_code_v2/cs-acryl-example-scripts/circuit_break_examples/graphql/"
 
 
-def get_graph_query(file_path):
+def get_graph_query(file_path: str) -> str:
     with open(file_path, "r") as f:
         query = f.read()
     return query
 
 
-def get_upstreams(graph):
-
+def get_upstreams(graph: DataHubGraph) -> Dict[str, Any]:
     variables = get_scroll_across_lineage_vars(
         "urn:li:dataset:(urn:li:dataPlatform:postgres,postgres.public.testtable,PROD)",
         "UPSTREAM",
@@ -21,66 +27,45 @@ def get_upstreams(graph):
     )
     gql_query = get_graph_query(GRAPHQL_QUERY_FOLDER_PATH + "scrollAcrossLineage.gql")
     response = graph.execute_graphql(query=gql_query, variables=variables)
-
     return response
 
 
-def get_dataset_assertions(graph):
-
+def get_dataset_assertions(graph: DataHubGraph) -> Dict[str, Any]:
     variables = get_dataset_assertions_vars(
         "urn:li:dataset:(urn:li:dataPlatform:redshift,rs.dokken.prod.dokken_prod.wba_events.core_heartbeat,PROD)"
     )
-
     gql_query = get_graph_query(GRAPHQL_QUERY_FOLDER_PATH + "getDatasetAssertions.gql")
-
     response = graph.execute_graphql(query=gql_query, variables=variables)
-
     return response
 
 
-def add_tag(graph, entity_urn, tag_urn):
-
+def add_tag(graph: DataHubGraph, entity_urn: str, tag_urn: str) -> Dict[str, Any]:
     variables = get_add_tag_vars(entity_urn, tag_urn)
-
     gql_query = get_graph_query(GRAPHQL_QUERY_FOLDER_PATH + "addTag.gql")
-
     response = graph.execute_graphql(query=gql_query, variables=variables)
-
     return response
 
 
-def remove_tag(graph, entity_urn, tag_urn):
-
+def remove_tag(graph: DataHubGraph, entity_urn: str, tag_urn: str) -> Dict[str, Any]:
     variables = get_remove_tag_vars(entity_urn, tag_urn)
-
     gql_query = get_graph_query(GRAPHQL_QUERY_FOLDER_PATH + "removeTag.gql")
-
     response = graph.execute_graphql(query=gql_query, variables=variables)
-
     return response
 
 
-def get_assertion_details(graph, assertion_urn):
-
+def get_assertion_details(graph: DataHubGraph, assertion_urn: str) -> Dict[str, Any]:
     variables = get_assertion_details_vars(assertion_urn)
-
     gql_query = get_graph_query(
         GRAPHQL_QUERY_FOLDER_PATH + "getAssertionWithMonitors.gql"
     )
-
     response = graph.execute_graphql(query=gql_query, variables=variables)
-
     return response
 
 
-def run_assertion(graph, assertion_urn):
-
+def run_assertion(graph: DataHubGraph, assertion_urn: str) -> Dict[str, Any]:
     variables = get_run_assertion_vars(assertion_urn)
-
     gql_query = get_graph_query(GRAPHQL_QUERY_FOLDER_PATH + "runAssertion.gql")
-
     response = graph.execute_graphql(query=gql_query, variables=variables)
-
     return response
 
 
@@ -93,7 +78,6 @@ graph = DataHubGraph(DatahubClientConfig(server=gms_endpoint, token=token))
 # Execute the query
 upstreams_object = get_upstreams(graph)
 assertion_object = get_dataset_assertions(graph)
-
 
 assertions = assertion_object["dataset"]["assertions"]["assertions"]
 
